9793f6c3bf023c387e30004f005216d34f15af9f
Daksh-bala69 <dakshnonjuvenile@gmail.com>
2026-06-04 00:13:26 +0530

    made the progressbar dragable and the functions are more compact and meaning with the new 'loadSong function and the queue is now a js object , ill later impelent the next and previous duttons to work and the queue to change according to it , shoudnt take too long'

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
+const songs =[
+  {
+    title: "Good Life",
+    artist : "Kanye West",
+    audio: "./assets/audio/05 - Good Life.flac",
+    cover: "./assets/images/Kanye-Graduation-blank.png",
+    duration: "3:23"
+  },
+  {
+    title: "Stronger",
+    artist : "Kanye West",
+    audio: "./assets/audio/03 - Stronger.flac",
+    cover: "./assets/images/Kanye-Graduation-blank.png",
+    duration: "5:12"
+  },
+  {
+    title: "Flashing Lights",
+    artist : "Kanye West",
+    audio: "./assets/audio/09 - Flashing Lights.flac",
+    cover: "./assets/images/Kanye-Graduation-blank.png",
+    duration: "3:57"
+  },
+  {
+    title: "Homecoming",
+    artist : "Kanye West",
+    audio: "./assets/audio/12 - Homecoming.flac",
+    cover: "./assets/images/Kanye-Graduation-blank.png",
+    duration: "3:24"
+  }
+];
+
+
 let isPlaying = false;
 
-let currSong = new Audio("./assets/audio/05 - Good Life.flac");
+let currSongIndex = 0;
+let currSong = new Audio(songs[currSongIndex].audio);
 //CURRENT TIME AND THE DURATION OF THE SONG (CURRENT TIME NEEDS TO UPDATED FREQUENTLY )
+const currentTime = document.getElementById("currentTime");
 
 function setDuration(){
-  const currentTime = document.getElementById("currentTime");
   const duration = document.getElementById("duration");
 
   currSong.addEventListener("loadedmetadata", function () {
@@ -15,6 +48,7 @@ function setDuration(){
       durationMinutes + ":" + String(durationSeconds).padStart(2, "0");
   });
 }
+setDuration();
 
 const mainPlayButton = document.getElementById("mainPlayButton");
 const bottomPlayButton = document.getElementById("bottomPlayButton");
@@ -35,6 +69,43 @@ const mainSongArtist= document.getElementById("mainSongArtist");
 console.log(mainPlayerCover);
 console.log(mainSongArtist);
 console.log(mainSongTitle);
+
+
+const queueItems = document.querySelectorAll(".queueItem");
+
+function loadSong(index) {
+// CHANGING LOGIC INFO
+  const song = songs[index];
+  currSong = new Audio(song.audio);
+  currSongIndex = index;  
+// CHANGING THE CONTENTS DISPLAYED
+
+  mainSongTitle.textContent = song.title;
+  mainSongArtist.textContent = song.artist;
+  mainPlayerCover.src = song.cover;
+
+  bottomSongTitle.textContent = song.title;
+  bottomSongArtist.textContent = song.artist;
+  bottomAlbumCover.src = song.cover;
+
+// SETTING THE DURATION;
+  setDuration();
+  progressFill.style.width = "0%";
+  currentTime.textContent = "0:00";
+  
+//CHANGING THE ACTIVE QUEUE MEMBER
+  queueItems.forEach((item, idx) => {
+    item.classList.remove("active");
+    const itemSpan = queueItems[idx].querySelector("span");
+    itemSpan.textContent = songs[idx].duration;
+  });
+  queueItems[index].classList.add("active");
+  queueItems[index].querySelector(".songTime").textContent = "▶";
+
+};
+
+loadSong(0);
+
 
 function updatePlayButtons() {
   if (isPlaying) {
@@ -78,7 +149,7 @@ setInterval(function() {
   }
 
 //MAKING SURE THAT THE CURRENT TIME DOESNT SHOW NaN
-  if(isPlaying && duration){
+  if(duration){
     const minutes = Math.floor(currSong.currentTime / 60);
     const seconds = Math.floor(currSong.currentTime % 60);
     
@@ -90,42 +161,60 @@ setInterval(function() {
 
 //CHANGING QUEUE
 
-const queueItems = document.querySelectorAll(".queueItem");
 
-queueItems.forEach((item) => {
+queueItems.forEach((item, index) => {
   item.addEventListener("click" , function () {
-    queueItems.forEach((song) =>{
-      song.classList.remove("active");
-      
-      const songTime = song.querySelector(".songTime");
-      if(songTime.textContent === "▶") {
-        songTime.textContent = song.dataset.time;
-      }
-    });
-    console.log(item.dataset.audio);
-
     currSong.pause();
-    currSong = new Audio(item.dataset.audio);
+    loadSong(index);
+
     isPlaying = true;
-
-    setDuration();
     updatePlayButtons();
-    item.classList.add("active");
-    item.querySelector(".songTime").textContent = "▶";
-
-    //UPDATING THE BOTTOM PLAYER (These obj's were defined at the beginning)
-    const queueSongInfo = item.querySelector(".queueSongInfo");
-
-    bottomSongTitle.textContent = queueSongInfo.querySelector("h4").textContent;
-    bottomSongArtist.textContent = queueSongInfo.querySelector("p").textContent;
-    bottomAlbumCover.src = item.querySelector("img").src;
-    
-    //UPDATING THE MAIN PLAYER (These obj's were defined at the beginning)
-    mainSongTitle.textContent = queueSongInfo.querySelector("h4").textContent;
-    mainSongArtist.textContent = queueSongInfo.querySelector("p").textContent;
-    mainPlayerCover.src = item.querySelector("img").src;
-
-    progress = 0;
-    progressFill.style.width = "0%";
   });
 });
+
+// FOR CLICKING THE PROGRESS BAR TO CHANGE DURATION FO THE SONG
+const progressBar = document.querySelector(".progressBar");
+
+
+function seek(e) {
+
+  if (!currSong.duration) return;
+  const rect = progressBar.getBoundingClientRect();
+
+  let clickX = e.clientX - rect.left;
+  let percent = clickX / rect.width;
+
+  percent = Math.max(0, Math.min(1, percent));
+
+  currSong.currentTime = percent * currSong.duration;
+  progressFill.style.width = percent * 100 + "%";
+  
+}
+
+progressBar.addEventListener("click", function (e) {
+  seek(e);
+});
+
+// FOR DRAGGING THE PROGRESS BAR
+let isDragging = false;
+
+
+progressBar.addEventListener("mousedown", function (e) {
+  isDragging = true;
+  seek(e);
+});
+
+document.addEventListener("mousemove", function (e) {
+  if (isDragging) {
+    seek(e);
+  }
+});
+
+document.addEventListener("mouseup", function () {
+  isDragging = false;
+});
+
+
+const rect = progressBar.getBoundingClientRect();
+console.log(rect);
+
